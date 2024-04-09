0e9bc59aec702741d79fcc0edc3340fc9ea9fa91
RecodeLiner <RecodeLIner@gmail.com>
2024-04-09 19:06:31 +0300

    Update gradle file

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,52 +1,44 @@
 plugins {
+    id("maven-publish")
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
 }
 
+val javaVer = JavaVersion.VERSION_17
+
 android {
     compileSdkVersion = "android-31"
+    namespace = "com.lizowzskiy.accents"
     defaultConfig {
         minSdk = 23
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = javaVer
+        targetCompatibility = javaVer
     }
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
 }
 
 kotlin {
-//    targetHierarchy.default()
-    jvmToolchain(17)
+    applyDefaultHierarchyTemplate()
+    jvmToolchain("$javaVer".toInt())
 
-    jvm {
-
-    }
+    jvm()
 
     androidTarget {
         publishLibraryVariants("release")
         compilations.all {
             kotlinOptions {
-                jvmTarget = "17"
+                jvmTarget = "$javaVer"
             }
         }
     }
 
-//    linuxX64()
-
-//    macosX64()
-//    macosArm64()
-
     sourceSets {
-        val commonMain by getting {
-            dependencies {
-
-            }
-        }
         val commonTest by getting {
             dependencies {
-                implementation(libs.kotlin.test)
+                implementation(kotlin("test"))
             }
         }
 
@@ -55,17 +47,5 @@ kotlin {
                 implementation(libs.apache.commonLang)
             }
         }
-
-//        val androidMain by getting {
-//
-//        }
-    }
-}
-
-android {
-    namespace = "com.lizowzskiy.accents"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
     }
 }
